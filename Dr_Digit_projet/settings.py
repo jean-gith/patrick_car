019888f28ff22cfe2c44e7c77db54c95bffd8fb2
Ouangui <patrickouangui@gmail.com>
2021-02-01 14:39:16 +0000

    make it better

--- Dr_Digit_projet/settings.py
+++ Dr_Digit_projet/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'drdigitcar', # le nom de notre base de donnees creee precedemment
         'USER': 'postgres', # attention : remplacez par votre nom d'utilisateur
         'PASSWORD': 'Ouangui137955',
-        'HOST': '127.0.0.1  ',
+        'HOST': '127.0.0.1',
         'POST': '5432',
     }
 }
