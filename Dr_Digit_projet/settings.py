118bbcd00828ae9bd5469698c8fb3cd1c4c4df23
Ouangui <patrickouangui@gmail.com>
2021-02-01 14:50:17 +0000

    make it better

--- Dr_Digit_projet/settings.py
+++ Dr_Digit_projet/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mycarapp/static/media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'mycarapp/static/media')
 
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
 
